b4d685bbde1a0514d061e9ae264bf487716a4c16
Robert Walker <robertwalker@Roberts-MacBook-Air.local>
2013-02-03 07:26:30 -0800

    kkkk

---
+++ Gittest/GittestLabelViewController.h
@@ -0,0 +1,13 @@
+//
+//  GittestLabelViewController.h
+//  Gittest
+//
+//  Created by Robert Walker on 2/3/13.
+//  Copyright (c) 2013 Robert Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GittestLabelViewController : UIViewController
+
+@end
